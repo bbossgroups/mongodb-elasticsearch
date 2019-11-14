be6f98606abc49828273406070a263e1e66317f1
yinbp <yin-bp@163.com>
2019-11-14 11:18:06 +0800

    http组件增加慢请求服务调用日志信息记录，增加慢sql时间条件参数配置和推送慢请求调用信息回调函数 slowDslThreshold单位：ms 毫秒 elasticsearch.slowDslThreshold=10000 spring boot配置项 spring.elasticsearch.bboss.elasticsearch.slowDslThreshold=10000
    需要截取掉过长的dsl:超过2048的dsl将被截取掉超过的内容，替换为.....

--- src/main/java/org/frameworkset/elasticsearch/imp/QuartzTimestampImportTask.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/QuartzTimestampImportTask.java
@@ -71,14 +71,9 @@ public class QuartzTimestampImportTask extends AbstractDB2ESQuartzJobHandler {
 					.setIndexType("mongodbdemo") //es 7以后的版本不需要设置indexType，es7以前的版本必需设置indexType
 //				.setRefreshOption("refresh")//可选项，null表示不实时刷新，importBuilder.setRefreshOption("refresh");表示实时刷新
 					.setPrintTaskLog(true) //可选项，true 打印任务执行日志（耗时，处理记录数） false 不打印，默认值false
-					.setBatchSize(10);  //可选项,批量导入es的记录数，默认为-1，逐条处理，> 0时批量处理
+					.setBatchSize(10)  //可选项,批量导入es的记录数，默认为-1，逐条处理，> 0时批量处理
+					.setFetchSize(100); //按批从mongodb拉取数据的大小
 
-			//定时任务配置，
-			importBuilder.setFixedRate(false)//参考jdk timer task文档对fixedRate的说明
-//					 .setScheduleDate(date) //指定任务开始执行时间：日期
-					.setDeyLay(1000L) // 任务延迟执行deylay毫秒后执行
-					.setPeriod(5000L); //每隔period毫秒执行，如果不设置，只执行一次
-			//定时任务配置结束
 //
 //		//设置任务执行拦截器，可以添加多个，定时任务每次执行的拦截器
 //		importBuilder.addCallInterceptor(new CallInterceptor() {
